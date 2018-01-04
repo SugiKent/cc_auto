3ccb9fc5cbdfbaa9dc5671e6eacccc36d82738ec
SugiKen <ynmk1995@gmail.com>
2018-01-05 01:49:06 +0900

    productionでのみ、実行する

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,9 +1,9 @@
 class TopController < ApplicationController
   def index
+    # 以下テスト用
     Currency.get_rates
-
     trans = Transaction.new
-    if Rails.env == 'development'
+    if Rails.env == 'production'
       trans.sell_buy_coin
     end
 
