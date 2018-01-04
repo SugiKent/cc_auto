629b56055d59e9f966dccfc2700db8d46477d512
SugiKen <ynmk1995@gmail.com>
2018-01-04 15:08:58 +0900

    売買ロジック修正

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -91,7 +91,7 @@ class Transaction < ApplicationRecord
 
       puts "過去のbtcの平均値は、#{bitcoins_avg}円\n現在のレートは、#{now_rate['rate']}円"
 
-      which = now_rate['rate'].to_i > bitcoins_avg || now_rate['rate'].to_i > past_trans.rate + 3000
+      which = now_rate['rate'].to_i > bitcoins_avg || now_rate['rate'].to_i > past_trans.rate + 30000
       puts "判定の結果：売りは#{which}"
       which
     elsif past_trans.order_type == 'sell'
@@ -113,7 +113,7 @@ class Transaction < ApplicationRecord
         puts '200万円を超えているため、購入を見送りました。'
         return false
       else
-        which = now_rate['rate'].to_i < bitcoins_avg + 1000 && now_rate['rate'].to_i < past_trans.rate - 2000
+        which = now_rate['rate'].to_i < bitcoins_avg + 10000 && now_rate['rate'].to_i < past_trans.rate - 20000
         puts "判定の結果：購入は#{which}"
         which
       end
