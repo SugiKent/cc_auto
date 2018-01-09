7860058a25b64902905cef8f207908fb1a5dac80
SugiKen <ynmk1995@gmail.com>
2018-01-09 12:01:01 +0900

    最低値を通知するプログラムを実装

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,4 +1,7 @@
 require 'net/http'
+require 'uri'
+require 'openssl'
+require 'json'
 
 class Currency < ApplicationRecord
   self.inheritance_column = :_type_disabled
@@ -44,5 +47,50 @@ class Currency < ApplicationRecord
 
     sum_all = currencies.pluck(:rate).sum / currencies.count
 
+  end
+
+  def compare_lowest_rate
+    Currency.types.keys.each do |type|
+      uri = URI.parse "https://coincheck.com/api/rate/#{type}_jpy"
+      json = Net::HTTP.get(uri)
+      result = JSON.parse(json)
+
+      next if result.blank?
+
+      lowest_rate = lowest_rate_1day(type)
+
+      # 現在の価格が24時間以内の最低値の場合、trueを返す
+      if result['rate'].to_i < lowest_rate.rate
+        notify_lowest_rate(type,result['rate'].to_i)
+      end
+    end
+  end
+
+  def notify_lowest_rate(type,rate)
+    msg = "【bot】#{type}が24時間以内で最低値になりました。\n\n現在のレート：#{rate}円/#{type}\n\ncoincheckのデータより"
+    line_notify(msg)
+  end
+
+  def lowest_rate_1day(type)
+    lowest_rate = Currency.where(type: type).where("currencies.created_at > ?", DateTime.now - 1.days).order(:rate).first
+  end
+
+  def line_notify(msg)
+    uri = URI.parse("https://notify-api.line.me/api/notify")
+
+    request = make_request(msg)
+    response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |https|
+      https.request(request)
+    end
+  end
+
+  def make_request(msg)
+    token = ENV['LINE_TOKEN']
+    uri = URI.parse("https://notify-api.line.me/api/notify")
+
+    request = Net::HTTP::Post.new(uri)
+    request["Authorization"] = "Bearer #{token}"
+    request.set_form_data(message: msg)
+    request
   end
 end
